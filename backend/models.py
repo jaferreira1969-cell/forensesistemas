7aa4fa33185b57d23633783ed4f99a73a683d544
jaferreira1969-cell <jaferreira1969@gmail.com>
2025-11-30 11:12:57 -0300

    Fix database import in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from database import Base
+from backend.database import Base
 
 class Operacao(Base):
     __tablename__ = "operacoes"
